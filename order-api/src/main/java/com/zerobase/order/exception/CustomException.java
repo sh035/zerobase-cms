eee627d7f9830615bc574b4dded45084f2165990
sh035 <saab35@naver.com>
2024-12-29 03:38:48 +0900

    feat: 여태까지 했던거 저장

--- order-api/src/main/java/com/zerobase/order/exception/CustomException.java
+++ order-api/src/main/java/com/zerobase/order/exception/CustomException.java
@@ -1,12 +1,11 @@
 package com.zerobase.order.exception;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
 
 @Getter
 @RequiredArgsConstructor
-public class CustomException extends RuntimeException{
+public class CustomException extends RuntimeException {
     private final ErrorCode errorCode;
     private final int status;
     private static final ObjectMapper mapper = new ObjectMapper();
@@ -15,5 +14,15 @@ public class CustomException extends RuntimeException{
         super(errorCode.getDetail());
         this.errorCode = errorCode;
         this.status = errorCode.getHttpStatus().value();
+    }
+
+    @AllArgsConstructor
+    @Builder
+    @NoArgsConstructor
+    @Getter
+    public static class CustomExceptionResponse {
+        private int status;
+        private String code;
+        private String message;
     }
 }
